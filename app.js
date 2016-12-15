53de5c4daf4f850d4ab5b133721542d4d496a24d
Sirajudheen Mohamed Ali <sirajudheen.mohamed.ali@sap.com>
2016-12-15 18:36:03 +0100

    name changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 // Database
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest2');
+var db = monk('localhost:27017/node_db_app');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
